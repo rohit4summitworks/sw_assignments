d96df4af7a5816a35e31155e1e1c5b2057764416
rohit4summitworks <rohit.yadav@summitworks.com>
2023-04-18 21:51:57 +0530

    Update Test.java

--- CoreJava/src/Test.java
+++ CoreJava/src/Test.java
@@ -5,6 +5,8 @@ public class Test {
 		// TODO Auto-generated method stub
 		System.out.println("Test");
 		System.out.println("New code");
+		int a=10,b=20;
+		System.out.println("Add="+(a+b));
 	}
 
 }
